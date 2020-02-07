b911cb12dc791ff278314d036ef9748bd387ef6d
Tyrel Alastair Hunter <tyrel.hunter@pointepestcontrol.net>
2020-02-07 15:12:19 -0700

    Add project files.

---
+++ MicrosoftGraphEmailSender/GraphEmailSender.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Threading.Tasks;
+
+namespace MicrosoftGraphEmailSender
+{
+    public class GraphEmailSender
+    {
+        private GraphServiceClient Client { get; set; }
+        public GraphEmailSender(string clientId, string clientSecret, string tenantId)
+        {
+            var authProvider = new MicrosoftGraphAuthenticationProvider(clientId, clientSecret, new string[] { "https://graph.microsoft.com/.default" }, tenantId);
+            Client = new GraphServiceClient(authProvider);
+        }
+
+        public async Task SendMailAsync(string from, string to, string message, bool isHTML = false)
+        {
+            ItemBody content = CreateItemBody(message, isHTML);
+            Message msg = CreateMessage(from, to, content);
+            await Client.Users[from].SendMail(msg, true).Request().PostAsync();
+        }
+
+        public void SendMail(string from, string to, string message, bool isHTML = false)
+        {
+            SendMailAsync(from, to, message, isHTML).Wait();
+        }
+
+        public void SendMail(string from, Message message)
+        {
+            SendMailAsync(from, message).Wait();
+        }
+
+        public async Task SendMailAsync(string from, Message message)
+        {
+            await Client.Users[from].SendMail(message, true).Request().PostAsync();
+        }
+
+        private Message CreateMessage(string from, string to, ItemBody content)
+        {
+            Message msg = new Message();
+            msg.Body = content;
+            msg.ToRecipients = new Recipient[] { new Recipient() { EmailAddress = new EmailAddress() { Address = to } } };
+            return msg;
+
+        }
+        private ItemBody CreateItemBody(string content, bool isHtml = false)
+        {
+            ItemBody body = new ItemBody();
+            body.ContentType = isHtml ? BodyType.Html : BodyType.Text;
+            body.Content = content;
+            return body;
+        }
+    }
+}
---
+++ MicrosoftGraphEmailSender/MicrosoftGraphAuthenticationProvider.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftGraphEmailSender
+{
+    public class MicrosoftGraphAuthenticationProvider : IAuthenticationProvider
+    {
+
+        private readonly string clientId;
+        private readonly string clientSecret;
+        private readonly string[] appScopes;
+        private readonly string tenantId;
+
+        public MicrosoftGraphAuthenticationProvider(string clientId, string clientSecret, string[] appScopes, string tenantId)
+        {
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+            this.appScopes = appScopes;
+            this.tenantId = tenantId;
+        }
+
+        public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+        {
+            var clientApplication = ConfidentialClientApplicationBuilder.Create(this.clientId)
+                .WithClientSecret(this.clientSecret)
+                .WithTenantId(this.tenantId)
+                .Build();
+
+            var result = await clientApplication.AcquireTokenForClient(this.appScopes).ExecuteAsync();
+
+            request.Headers.Add("Authorization", result.CreateAuthorizationHeader());
+        }
+    }
+}
